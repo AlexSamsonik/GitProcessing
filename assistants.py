2a82a04ed19a6d6b05d0e963a6bca23772116f04
Aliaksandr Samsonik <aliaksandr_samsonik@epam.com>
2023-07-10 12:27:52 +0200

    Docstring and typing has been added to assistants.py

--- assistants.py
+++ assistants.py
@@ -1,8 +1,17 @@
+"""This module provides utility functions for running commands using subprocess."""
+
 import logging
 from subprocess import Popen, PIPE
 
 
-def run_command(command, cwd=None):
+def run_command(command: str, cwd: str = None) -> tuple:
+    """
+    Run a command using subprocess.
+
+    :param command: The command to run.
+    :param cwd: The working directory in which to run the command. (default: None)
+    :return: A tuple containing the process object, the command output, and the error output.
+    """
     process = Popen(command.split(), cwd=cwd, stdout=PIPE, stderr=PIPE)
     output, error = process.communicate()
     if output:
